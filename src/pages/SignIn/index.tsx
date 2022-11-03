3ab7d79ae795c0481b8f2a6a4dd79423ed365bcc
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-11-03 15:34:15 -0300

    usenavigate

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,62 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { ScrollView, KeyboardAvoidingView, Platform, View } from 'react-native';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
-import { Container, Content, Title } from './styles';
+import {
+  Container,
+  Content,
+  CreateAccount,
+  CreateAccountTitle,
+  ForgotPasswordButton,
+  ForgotPasswordTitle,
+  Icon,
+  Logo,
+  Title,
+} from './styles';
+import logo from '../../assets/logo.png';
+
+interface ScreenNavigationProp {
+  navigate: (screen: string) => void;
+}
 
 export const SignIn: React.FC = () => {
+  const { navigate } = useNavigation<ScreenNavigationProp>();
+
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
+    <KeyboardAvoidingView
+      /* Aplicase ao ios para não cobrir a tela com o teclado */
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <Container>
-        <Content>
-          <Title>Faça seu login</Title>
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
-          <Button title="Entrar"></Button>
-        </Content>
-      </Container>
-    </ScrollView>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Content>
+            <Logo source={logo}></Logo>
+            <View>
+              <Title>Faça seu login</Title>
+            </View>
+            <Input placeholder="Email" />
+            <Input placeholder="Senha" />
+            <Button title="Entrar"></Button>
+            <ForgotPasswordButton>
+              <ForgotPasswordTitle>Esqueci minha senha</ForgotPasswordTitle>
+            </ForgotPasswordButton>
+          </Content>
+        </Container>
+      </ScrollView>
+      <CreateAccount
+        onPress={() => {
+          navigate('SignUp');
+        }}
+      >
+        <Icon name="log-in" />
+        <CreateAccountTitle>Criar uma conta</CreateAccountTitle>
+      </CreateAccount>
+    </KeyboardAvoidingView>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
-import { Container, Content, Title } from './styles';
+import {
+  BackToSignIn,
+  BackToSignInTitle,
+  Container,
+  Content,
+  Icon,
+  Logo,
+  Title,
+} from './styles';
+import logo from '../../assets/logo.png';
+
+interface ScreenNavigationProp {
+  goBack: () => void;
+}
 
 export const SignUp: React.FC = () => {
+  const { goBack } = useNavigation<ScreenNavigationProp>();
+
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <Container>
-        <Content>
-          <Title>Crie sua conta</Title>
-          <Input placeholder="Nome completo" />
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
-          <Button title="Criar conta"></Button>
-        </Content>
-      </Container>
-    </ScrollView>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Content>
+            <Logo source={logo} />
+            <Title>Crie sua conta</Title>
+            <Input placeholder="Nome completo" />
+            <Input placeholder="Email" />
+            <Input placeholder="Senha" />
+            <Button title="Criar conta"></Button>
+          </Content>
+        </Container>
+      </ScrollView>
+      <BackToSignIn>
+        <Icon name="arrow-left" />
+        <BackToSignInTitle onPress={() => goBack()}>
+          Voltar para Logon
+        </BackToSignInTitle>
+      </BackToSignIn>
+    </KeyboardAvoidingView>
   );
 };
