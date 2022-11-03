db819eead4cd85557a5b43f05a106898c899252e
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-11-03 08:16:15 -0300

    projeto react native

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Container, Title } from './styles';
+
+export const Home: React.FunctionComponent = () => {
+  return (
+    <Container>
+      <Title>Home Page</Title>
+    </Container>
+  );
+};
